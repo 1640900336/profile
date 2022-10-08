879667a35538b1c7b69dd77da93e54f5b198b20b
1640900336 <112235614+1640900336@users.noreply.github.com>
2022-10-08 21:32:47 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <Navbar />
+    <section class="home">
+
+    <div class="image">
+        <img src="https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/288559449_1436527996843444_4328702160863813768_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Sr6yilPLc1wAX9O11zd&tn=UJ3VVU7uHZqpD2g0&_nc_ht=scontent.fbkk13-2.fna&oh=00_AT8D41Jq_9k7w6SblLd4-sSSPxrkBzCZrl8zmEo9s_SC-Q&oe=63461E69" alt="">
+    </div>
+
+    <div class="content">
+        <h3>hi, i am Thanandorn Wannaket</h3>
+        <span>Belive the Impossible is Possible.</span>
+        <p>More 5 Years of Experienced Developer with Front-end and Back-end .NET programming in the information technology and service industry. Skilled in HTML, CSS, Javascript, Angular, React, MVC, C# and ORACLE, SQL database tools. A strong engineering professional with a Bachelor of Engineering (BE) focused on Computer Engineering from Bangkok University.</p>
+        <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+    </div>
+
+</section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export default {
+    components: { Navbar }
+}
+</script>
+
+<style>
+
+</style>
